6041bfff854449a0efd6d45db0723291f0d4f77d
samigmuseyibli <samiqmuseyibli@gmail.com>
2025-12-24 18:11:53 +0400

    merge fixed

--- application/controllers/Strategic_plan.php
+++ application/controllers/Strategic_plan.php
@@ -8,10 +8,7 @@ class Strategic_plan extends CI_Controller
     {
 
         parent::__construct();
-<<<<<<< HEAD
         header('X-Frame-Options: SAMEORIGIN');
-=======
->>>>>>> 4cc5aa09946e2d7d90543c375f38a44cdd79c424
         if (!$this->session->userdata('language')) {
             $lang = $this->db->get_where('general_settings', array('type' => 'site_language'))->row()->value;
             $this->session->set_userdata('language', $lang);
@@ -22,12 +19,8 @@ class Strategic_plan extends CI_Controller
     public function index()
     {
 
-<<<<<<< HEAD
-        $l = curLang();
-=======
         $l=curLang();
 
->>>>>>> 4cc5aa09946e2d7d90543c375f38a44cdd79c424
         $data['title'] = translate('strategic_plan');
         $this->db->select('title_' . $l . '');
         $this->db->select('description_' . $l . '');
